f2316c16b86e34bdbb2a75f1598fa49698bb636a
IreneNSilvernail <insilver@ncsu.edu>
2026-07-07 16:38:45 -0400

    Update Setup.py

--- tutorials/MOFF-MRG_Nucleosome_Condensate/Simulation/Setup.py
+++ tutorials/MOFF-MRG_Nucleosome_Condensate/Simulation/Setup.py
@@ -14,6 +14,7 @@ from openabc.utils.insert import insert_molecules
 from openabc.forcefields.rigid import createRigidBodies
 from nucl_utils import get_MOFF_MRG_chromatin_rigid_bodies
 
+# Standard residue definitions for reference
 _amino_acids = ['ALA', 'ARG', 'ASN', 'ASP', 'CYS',
                 'GLN', 'GLU', 'GLY', 'HIS', 'ILE',
                 'LEU', 'LYS', 'MET', 'PHE', 'PRO',
@@ -22,17 +23,35 @@ _amino_acids = ['ALA', 'ARG', 'ASN', 'ASP', 'CYS',
 _nucleotides = ['DA', 'DT', 'DC', 'DG']
 
 
-# set slab simulation box size
+# ==============================================================================
+# 1. GLOBAL SIMULATION PARAMETERS & GEOMETRY
+# ==============================================================================
+# Define dimensions for the slab geometry (in nm) used to study phase separation
 box_a = 49
 box_b = 49
-box_c = 300
+box_c = 300 # Extended z-axis creates the slab interface
 
-# we have to rebuild the system as this time there is no MonteCarloBarostat in it
+# Total number of nucleosomes in the system
+n_nucl=100
+
+# Environmental variables
+salt_conc = 200 * unit.millimolar
+temperature = 300 * unit.kelvin
+manning_scale = 1.0
+
+
+# ==============================================================================
+# 2. PARSE MONOMER STRUCTURES & INITIALIZE FORCEFIELDS
+# ==============================================================================
+# Parse the histone octamer core using the MOFF protein forcefield (C-alpha resolution)
 histone = MOFFParser.from_atomistic_pdb('starting_structure/histone.pdb', 'single_histone_CA.pdb', default_parse=False)
 histone.parse_mol(get_native_pairs=False)
+# Parse the double-stranded DNA linker/wrap using the MRG model
+# Note: bonded_energy_scale=0.8 matches the experimental 50 nm DNA persistence length
 dna = MRGdsDNAParser.from_atomistic_pdb('starting_structure/dna.pdb', 'cg_single_nucl_dna.pdb', default_parse=False)
 dna.parse_mol(bonded_energy_scale=0.8)
 
+# Assemble a single coarse-grained nucleosome unit for reference
 single_nucl = MOFFMRGModel()
 single_nucl.append_mol(histone)
 single_nucl.append_mol(dna)
@@ -40,57 +59,62 @@ single_nucl.atoms_to_pdb('cg_single_nucl.pdb')
 
 n_atoms_per_single_nucl = len(single_nucl.atoms.index)
 
-# Number of nucleosomes
-n_nucl = 100
-
+# ==============================================================================
+# 3. BUILD THE MULTI-NUCLEOSOME SYSTEM
+# ==============================================================================
+# Replicate the single nucleosome structure into the slab simulation box volume
 insert_molecules('cg_single_nucl.pdb', 'cg_n_nucl.pdb', n_mol=n_nucl, box=[box_a, box_b, box_c],reset_serial=False)
 
+# Build the composite OpenABC model container matching the number of nucleosomes
 multi_nucl = MOFFMRGModel()
 for i in range(n_nucl):
     multi_nucl.append_mol(histone)
     multi_nucl.append_mol(dna)
 
+# Generate OpenMM system topology from the generated multi-nucleosome PDB
 top = app.PDBFile('cg_n_nucl.pdb').getTopology()
 
 # Create a system to start the NVT simulation
 multi_nucl.create_system(top, box_a=box_a, box_b=box_b, box_c=box_c)
 
+# ==============================================================================
+# 4. SET UP RIGID BODIES & LOADING COORDINATES
+# ==============================================================================
+# Extract initial coordinates from a pre-equilibrated/compressed state
+npt_traj = mdtraj.load_dcd('../../../../equil-200mM/output_NPT.dcd', top='cg_n_nucl.pdb')
+init_coord = npt_traj.xyz[-1]
+
+# Center the geometric center of all coordinates inside the slab box bounds
+init_coord -= np.mean(init_coord, axis=0)
+init_coord += 0.5 * np.array([box_a, box_b, box_c])
+
+
 init_coord = app.PDBFile('./cg_n_nucl.pdb').getPositions()
-# Create the rigid bodies;
+# Define and register the rigid body groupings for each nucleosome (Histone core + 73 bp wrapped DNA)
 single_nucl_rigid_body = np.array(get_MOFF_MRG_chromatin_rigid_bodies(n_nucl=1, nrl=147, n_rigid_bp_per_nucl=73)[0])
 rigid_bodies = []
 for i in range(n_nucl):
     rigid_bodies.append((single_nucl_rigid_body + i*n_atoms_per_single_nucl).tolist())
 createRigidBodies(multi_nucl.system, init_coord, rigid_bodies)
 
-# load trajectory and get the compressed configuration
-# for easier visualization, we move the geometric center of all the atoms to the box center
-npt_traj = mdtraj.load_dcd('../../../../equil-200mM/output_NPT.dcd', top='cg_n_nucl.pdb')
-#npt_traj = mdtraj.load_dcd('./NPT_compress.dcd', top='cg_n_nucl.pdb')
-init_coord = npt_traj.xyz[-1]
-init_coord -= np.mean(init_coord, axis=0)
-init_coord += 0.5*np.array([box_a, box_b, box_c])
-
-# Start to run simulation;
-salt_conc = 200*unit.millimolar
-temperature = 300*unit.kelvin
-manning_scale = 1.0
+# ==============================================================================
+# 5. DEFINE POTENTIAL ENERGY TERMS & FORCE GROUPS
+# ==============================================================================
 multi_nucl.add_protein_bonds(force_group=1)
 multi_nucl.add_protein_angles(force_group=2)
-#multi_nucl.add_protein_dihedrals(force_group=3)
-#multi_nucl.add_native_pairs(force_group=4)
 multi_nucl.add_dna_bonds(force_group=5)
 multi_nucl.add_dna_angles(force_group=6)
 multi_nucl.add_dna_fan_bonds(force_group=7)
 multi_nucl.add_contacts(force_group=8)
-
 multi_nucl.add_elec_switch_map(salt_conc, manning_scale, temperature, cutoff1=4.0*unit.nanometer, cutoff2=4.3*unit.nanometer, force_group=9)
 
 # Save the system into system.xml
 multi_nucl.save_system('system.xml')
 
 
-# Begin the simulation
+# ==============================================================================
+# 6. INTEGRATOR CONFIGURATION & HARDWARE PLATFORM
+# ==============================================================================
 platform_name = 'CUDA'
 #platform_name = 'CPU'
 
@@ -101,6 +125,10 @@ integrator = mm.LangevinMiddleIntegrator(temperature, friction_coeff, timestep)
 multi_nucl.set_simulation(integrator, platform_name, init_coord=init_coord)
 simulation = multi_nucl.simulation
 
+# ==============================================================================
+# 7. REPORTERS & EXECUTION (NVT ENSEMBLE)
+# ==============================================================================
+# Clean up any overlapping initial coordinates before initiating steps
 simulation.minimizeEnergy()
 output_interval = 50000
 output_dcd = './traj_temp300.dcd'
@@ -118,7 +146,10 @@ print('Start NVT run.')
 simulation.step(5000000)
 print('NVT simulation is finished.')
 
-# print final box vectors
+# ==============================================================================
+# 8. POST-RUN DATA EXPORT
+# ==============================================================================
+# Query final state variables for system validation
 state = simulation.context.getState(getPositions=True, getVelocities=True, getForces=True, getEnergy=True,
                                     getParameters=True, enforcePeriodicBox=True)
 box_vec = state.getPeriodicBoxVectors(asNumpy=True)
